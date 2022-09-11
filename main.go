ca6ac9b1bf130d0958bea3a7d71e3cfe4bf063d6
nikimanoledaki <niki@weave.works>
2022-09-11 18:13:53 +0200

    date-setter updates deployment immediately

--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
-	"net/http"
+	"os"
+	"time"
 
-	"github.com/nikimanoledaki/date-setter/pkg/handlers"
-
-	"github.com/gorilla/mux"
+	"gopkg.in/yaml.v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	kyaml "k8s.io/apimachinery/pkg/util/yaml"
 )
 
 const (
-	port = ":8080"
+	defaultFilePath   = "build/default-deployment.yaml"
+	formattedFilePath = "clusters/formatted-deployment.yaml"
+	defaultTimeFormat = "01-02-2006-15-04-05"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/date", handlers.Date)
+	f, err := os.ReadFile(defaultFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	log.Printf("listening on port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, r))
+	decoder := kyaml.NewYAMLOrJSONDecoder(bytes.NewReader(f), 100)
+
+	for {
+		var obj unstructured.Unstructured
+		if err = decoder.Decode(&obj); err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+
+		formattedObj := setName(&obj)
+
+		newManifest, err := yaml.Marshal(formattedObj)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := os.WriteFile(formattedFilePath, []byte(newManifest), 0666); err != nil {
+			log.Fatal(err)
+		}
+	}
+	os.Exit(0)
+}
+
+func setName(d *unstructured.Unstructured) *unstructured.Unstructured {
+	t := time.Now()
+	d.SetName(fmt.Sprintf("podinfo-%v", t.Format(defaultTimeFormat)))
+	return d
 }
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,53 @@
 #!/bin/bash -ex
 
-# Create clusters
-eksctl create cluster -f snowflake-cluster.yaml
-eksctl create cluster -f gitops-cluster.yaml
+# Authenticate with AWS CLI
+~/exec/aws-creds.sh eksctl
+
+# Create Cluster 1 (snowflake cluster)
+cat > snowflake-cluster.yaml <<EOF
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+
+metadata:
+  name: snowflake-cluster
+  region: eu-north-1
+
+nodeGroups:
+  - name: ng-1
+    instanceType: t3.medium
+    desiredCapacity: 1
+
+
+EOF
+
+cat snowflake-cluster.yaml | eksctl create cluster --config-file -
+
+# Create Cluster 2 (GitOps cluster)
+cat > gitops-cluster.yaml <<EOF
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+
+metadata:
+  name: gitops-cluster
+  region: eu-north-1
+
+nodeGroups:
+  - name: ng-1
+    instanceType: t3.medium
+    desiredCapacity: 1
+EOF
+
+cat gitops-cluster.yaml | eksctl create cluster --config-file -
+
+# Setup Flux in Cluster 2
+flux bootstrap github --owner=$GITHUB_USER --repository=env-sus-gitops --path=clusters --private
+
+# Update Deployment name
+./name-setter
+
+# Apply changes to Cluster 1
+eksctl utils write-kubeconfig --cluster snowflake-cluster --region eu-north-1
+kubectl apply -f ../clusters/formatted-deployment.yaml
+
+# Push changes for Cluster 2
+git add . && git commit -m "Format date" && git push
