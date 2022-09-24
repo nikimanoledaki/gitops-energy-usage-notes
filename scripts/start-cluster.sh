42b9473205ba78ca912fa6e2258db16711db4139
nikimanoledaki <niki@weave.works>
2022-09-24 13:32:11 +0200

    Start cluster with kind

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
-minikube start --container-runtime=containerd --memory=6g --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.bind-address=0.0.0.0 --extra-config=controller-manager.bind-address=0.0.0.0
-minikube addons disable metrics-server
+# minikube start --container-runtime=containerd --memory=6g --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.bind-address=0.0.0.0 --extra-config=controller-manager.bind-address=0.0.0.0
+# minikube addons disable metrics-server
+#!/bin/bash
+
+# Install docker
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker $USER
+newgrp docker
+docker version
+sudo systemctl status docker
+
+# Install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.15.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# Start a kind cluster
+kind create cluster
+
+# Bootstrap Flux
 flux bootstrap github --owner=nikimanoledaki --repository=gitops-energy-usage --path=clusters
 
+while : ; do
+  kubectl get pod -l app.kubernetes.io/name=grafana && break
+  sleep 5
+done
+
+# Configure Grafana
 kubectl wait --for=condition=Ready=true pods -l app.kubernetes.io/name=grafana
 
 curl -fsSL "https://raw.githubusercontent.com/nikimanoledaki/gitops-energy-usage/main/scripts/grafana-kepler-dashboard.json" -o grafana-kepler-dashboard.json
